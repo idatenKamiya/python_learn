35597666b50836700880a963fbff4c5b062a217d
idatenKamiya <skandanadig.c@gmail.com>
2021-06-17 19:26:14 +0900

    Learning to make Loops and Control Flow

---
+++ structuring_data/script1.py
@@ -0,0 +1,10 @@
+#LISTS
+
+# Assign a list of strings to the fruits variable
+fruits = ['apple', 'banana', 'orange']
+
+# Print the element at index 0 
+print(fruits[0])
+
+# Concatenate the string and the element at index 2, and print the result
+print('I like ' + fruits[2] + 's')
